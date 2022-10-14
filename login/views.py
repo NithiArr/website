fc66a1ab19af0f940aad3d9836e0dbf0e07d2545
abc <abc@email.com>
2022-10-15 00:05:46 +0530

    login

--- login/views.py
+++ login/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from ast import Not
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 
 def loginn(request):
-    return render(request,'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user=auth.authenticate(username=username,password=password)
+        
+        if user is not None:
+            auth.login(request,user)
+            return render(request,'base.html')
+        else:
+            messages.info(request,"Username or password doent match !!!!")
+            return redirect('/')
+    else:
+        return render(request,'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
--- pro/urls.py
+++ pro/urls.py
@@ -1,12 +1,13 @@
 from django.contrib import admin
 from django.urls import path,include
 from home.views import home
-from login.views import loginn
-from base.views import stedit,stupdate,stdel
+from login.views import loginn,logout
+from base.views import stedit,stupdate,stdel,stinsert,stdisplay
 urlpatterns = [
-    path('',home),
-    path('login/',loginn),
-    path('show/',include('base.urls')),
+    path('',loginn),
+    path('logout/',logout),
+    path('create/',stinsert,name='stinsert'),
+    path('show/',stdisplay,name='stdisplay'),
     path('admin/', admin.site.urls),
     path('edit/<int:id>', stedit,name="stedit"),
     path('update/<int:id>',stupdate,name="stupdate"),
