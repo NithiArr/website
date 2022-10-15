87b6648f5f7c404433b8ee86aaf1434c8f896d30
abc <abc@email.com>
2022-10-16 00:49:52 +0530

    search

--- base/urls.py
+++ base/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('create',views.stinsert,name="stinsert"),
     path('edit/<int:id>', views.stedit,name="stedit"),
     path('update/<int:id>',views.stupdate,name="stupdate"),
+    path('search/<int:id>',views.search,name="search"),
     path('Delete/<int:id>',views.stdel,name="stdel")
 ]
--- pro/settings.py
+++ pro/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'home',
     'login',
-    'base'
+    'base',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
