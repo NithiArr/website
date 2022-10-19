684bc40182ad2504537e7a40f7eaa0d72e1e9da4
abc <abc@email.com>
2022-10-19 10:25:50 +0530

    s

--- home/views.py
+++ home/views.py
@@ -5,20 +5,21 @@ from django.db.models import Q
 
 def home(request):
     return render(request, 'home.html')
+
 def totalorder(request):
     results=crudst.objects.all()
-    return render(request,"totalorder.html",{"crudst":results})
+    return render(request,"home.html",{"crudst":results})
 
 def orderdelivered(request):
         status = 'delivered'
         emps = crudst.objects.all()
         if status:
             emps = emps.filter(Q(status__icontains = status))
-        return render(request, 'orderdeliver.html', {'detail': emps})
+        return render(request, 'home.html', {'detail': emps})
 
 def pending(request):
         status = 'pending'
         emps = crudst.objects.all()
         if status:
             emps = emps.filter(Q(status__icontains = status))
-        return render(request, 'orderdeliver.html', {'detail': emps})
+        return render(request, 'home.html', {'details': emps})
