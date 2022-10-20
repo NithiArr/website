8e742fb0020cfbc0ac09e8b77009ee6d8a896dc0
abc <abc@email.com>
2022-10-20 20:53:52 +0530

    error

--- login/views.py
+++ login/views.py
@@ -1,18 +1,21 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
-from django.contrib.auth.models import User, auth
+from django.contrib.auth import authenticate ,login ,logout
 
 def loginn(request):
     if request.method == "POST":
         username = request.POST['username']
         password = request.POST['password']
-        user = auth.authernticate(username=username , password=password)
+        user = authenticate(request,username=username , password=password)
+        
         if user is not None:
-            auth.login(request,user)
+            login(request,user)
             return redirect('home/')
         else:
             messages.info(request, 'Invalid')
-    else:
-        return render(request, 'login.html  ') 
+    return render(request, 'login.html') 
+
+
 def logout(request):
+
     return render(request,'/')
